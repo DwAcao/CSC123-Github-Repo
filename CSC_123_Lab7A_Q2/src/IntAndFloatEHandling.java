7e12721b339f92a67032262135734a768f022173
Alex Cao <acao4@toromail.csudh.edu>
2023-03-03 15:48:20 -0800

    -1st commit. Finished program, uploading to GitHub.

---
+++ CSC_123_Lab7A_Q2/src/IntAndFloatEHandling.java
@@ -0,0 +1,56 @@
+import java.util.*;
+/**
+ * 
+ * @author Alex C
+ * Write a program that accepts an integer a and a float b. The program should divide 
+ * a by b and output the result. 
+ * Anticipate and handle all exceptions and displays appropriate error messages. This will 
+ * include input validation as well as general exception handling.
+ */
+public class IntAndFloatEHandling
+{
+	public static void main(String[] args)
+	{
+
+		int a;	 float b;
+		Scanner KB = new Scanner(System.in);
+		
+		// Handling integer input validation
+		while(true) 
+		{
+			try {
+				System.out.print("Enter an integer: ");
+				a = KB.nextInt();
+				break;
+			}
+			catch(InputMismatchException IME) 
+			{
+				System.out.println("Invalid, enter a integer...");
+				KB.nextLine();
+			}
+		}
+		
+		// Handling float input validation
+		while(true) 
+		{
+			try {
+				System.out.print("Enter a float value: ");
+				b = KB.nextFloat();
+				break;
+			}
+			catch(InputMismatchException IME) 
+			{
+				System.out.println("Invalid, enter a float...");
+				KB.nextLine();
+			}
+		}
+		// Handling result validation.
+		try {
+			int result = a/(int)b;
+			System.out.print(a/b);
+		}
+		catch (ArithmeticException AE) {
+			System.out.println("Cannot divide by zero " + AE);
+		}
+	}
+}
